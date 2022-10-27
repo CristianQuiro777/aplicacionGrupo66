43aa69fde436f116d384dbaa117679bb2d435b1f
CristianQuiro777 <cristianquirogagt@gmail.com>
2022-10-26 20:13:12 -0500

    primera  version de  ejercicio ciclo 4

---
+++ app/src/main/java/com/example/aplicaciongrupo66/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.aplicaciongrupo66
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+class MainActivity: AppCompatActivity() {
+
+    private  var edtusername:  EditText? = null
+    private  var edtpassword : EditText? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun ingresar(botnlogin: View){
+
+        val intento= Intent(this,welcomActivity2::class.java)
+        startActivity(intento)
+
+    }
+}
